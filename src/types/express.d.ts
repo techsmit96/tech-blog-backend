28328d4cd6fbb877be9298730b65e89ea4b11711
Sumit <sumitsakpal02@gmail.com>
2025-07-07 20:56:39 +0530

    path added

---
+++ src/types/express.d.ts
@@ -0,0 +1,11 @@
+import { Request } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Express.Multer.File;
+      files?: Express.Multer.Files;
+      user?: any;
+    }
+  }
+}
--- src/v1/routes/route.mapping.ts
+++ src/v1/routes/route.mapping.ts
@@ -1,5 +1,17 @@
 import express from "express";
 const router = express.Router();
 
+import { auth } from "../../middleware/auth.middleware";
+
+import authRoutes from "./auth/auth.routes";
+import userRoutes from "./user/user.routes";
+import articleRoutes from "./article/article.routes";
+
+router.use("/auth", authRoutes);
+
+router.use(auth);
+
+router.use("/user", userRoutes);
+router.use("/article", articleRoutes);
 
 export default router;
